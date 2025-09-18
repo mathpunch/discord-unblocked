0158888754988b12f31abb896660e664861343b5
mathpunch Official <coutsozzy09@gmail.com>
2025-09-18 13:23:48 -0500

    Update uv.sw.js

--- static/uv/uv.sw.js
+++ static/uv/uv.sw.js
@@ -1,5 +1,5 @@
-importScripts('/uv/config.js');
-importScripts('/uv/bundle.js');
+importScripts('/uv/uv.bundle.js');
+importScripts('/uv/uv.config.js');
 
 class UVServiceWorker extends EventEmitter {   
     constructor(config = __uv$config) {
