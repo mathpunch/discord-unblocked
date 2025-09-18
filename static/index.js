4674d26aaf2d87010b6c53468e00c8c1059b2d55
mathpunch Official <coutsozzy09@gmail.com>
2025-09-18 13:28:46 -0500

    Update index.js

--- static/index.js
+++ static/index.js
@@ -7,7 +7,7 @@ form.addEventListener('submit', async event => {
         scope: __uv$config.prefix
     }).then(() => {
         let url = input.value.trim();
-        if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
+        if (!isUrl(url)) url = 'https://search.yahoo.com/search?q=' + url;
         else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
 
 
@@ -31,4 +31,5 @@ function openurl(inputurl){
         }
         location.href=__uv$config.prefix + __uv$config.encodeUrl(url);
     });
+
 }
